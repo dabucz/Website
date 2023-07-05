c79ac731672f5ac783dc3bf7c08a6e1d3b1ec516
dabucz <dabucuv@gmail.com>
2023-07-05 15:30:01 +0200

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 2024;
 var hostname = '127.0.0.1';
 
 app.listen(port, hostname, () => {
